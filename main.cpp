1ce5fce4193e95418bde8c6f47f272dc4fb0c826
mao <331060197@qq.com>
2018-03-26 17:22:56 +0800

    use own sift labrary but can't dubug

--- main.cpp
+++ main.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
-#include <opencv2/highgui.hpp>
+#include"sift/sift.h"
+#include <opencv2/opencv.hpp>  //头文件
+#include <opencv2/xfeatures2d.hpp>
+using namespace cv;  //包含cv命名空间
+using namespace std;
 
 
-int main(int argc, char *argv[])
+
+int main()
 {
-
-    cv::Mat img1 = cv::imread("../data/lena.png");
-    cv::Mat img2(img1);
-
-    //`Esc` drop out
-    while(cvWaitKey(33) != 27)
-    {
-        cv::imshow("main1",img2);
-    }
-
-    cvDestroyAllWindows();
-    img1.release();
-    img2.release();
-
-    return 0;
+    //Create SIFT class pointer
+    Ptr<Feature2D> f2d = xfeatures2d::q::SIFT::create();
+    //读入图片
+    Mat img_1 = imread("../data/img2.ppm");
+    Mat img_2 = imread("../data/img3.ppm");
+    //Detect the keypoints
+    vector<KeyPoint> keypoints_1, keypoints_2;
+    f2d->detect(img_1, keypoints_1);
+    f2d->detect(img_2, keypoints_2);
+    //Calculate descriptors (feature vectors)
+    Mat descriptors_1, descriptors_2;
+    f2d->compute(img_1, keypoints_1, descriptors_1);
+    f2d->compute(img_2, keypoints_2, descriptors_2);
+    //Matching descriptor vector using BFMatcher
+    BFMatcher matcher;
+    vector<DMatch> matches;
+    matcher.match(descriptors_1, descriptors_2, matches);
+    //绘制匹配出的关键点
+    Mat img_matches;
+    drawMatches(img_1, keypoints_1, img_2, keypoints_2, matches, img_matches);
+    cvNamedWindow("match",CV_WINDOW_NORMAL);
+    imshow("match", img_matches);
+    //等待任意按键按下
+    waitKey(0);
 }
